92af57be35054f9bfc5b5ce4240727f987ff9aab
64nyawira <charitynyawirairungu>
2024-05-07 15:28:35 +0300

    committing main.cs to CSHARP branch

---
+++ main.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double length = 5.0;
+        double width = 3.0;
+        double area = length * width;
+        Console.WriteLine("The area of the rectangle is: " + area);
+    }
+}
